4048f9ed04e1a76bd95f8486ba7d410a1fc595fd
cappyzawa <cappyzawa@yahoo.ne.jp>
2019-01-12 02:45:20 +0900

    add bcryptor

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bcryptor
 exec "golang.org/x/crypto/bcrypt" to strings of standard input
+
+## How to use
+```bash
+$ go get github.com/cappyzawa/bcryptor
+```
---
+++ cmd/bcryptor/main.go
@@ -0,0 +1,33 @@
+package bcryptor
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"io"
+	"os"
+)
+
+func input(r io.Reader) <-chan string {
+	ch := make(chan string)
+	go func() {
+		s := bufio.NewScanner(r)
+		for s.Scan() {
+			ch <- s.Text()
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func main() {
+	ch := input(os.Stdin)
+	for {
+		fmt.Print("input > ")
+		bs, err := bcrypt.GenerateFromPassword([]byte(<-ch), bcrypt.DefaultCost)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "err:", err)
+		}
+		fmt.Fprintln(os.Stdout, string(bs))
+	}
+}
